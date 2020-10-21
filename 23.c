ddbe1a2130a8b1a3296304b74d38ddc262fa2f5d
Rbn DEV <62131778+rbnndev@users.noreply.github.com>
2020-10-20 21:52:59 -0300

    Update 23.c

--- 23.c
+++ 23.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+
+// PLANEJAMENTO => CRIAR VARIAVEL I, J E AUX INTEIROS E CRIAR UMA VARIAVEL CHAR NOME PARA ARMAZENAR O NOME
+// SOLICITAR O NOME AO USUARIO, CRIAR UM FOR DO FINAL PARA O COMEÇO E ESCREVER OS CARACTERES DO ESPAÇO AO FIM
+// E PULAR LINHA, E UM WHILE NO FINAL PARA IMPRIMIR O PRIMEIRO NOME
 
 int main(void){
-    char nome[50];
-    int  i,x, count, e, f;
+	int i=0, j=0, aux=0;
+	char nome[100];
+	printf("Digite o nome completo: ");
+	gets(nome);
 
-    printf("Digite um nome:");
-    gets(nome);
-    
-    for(i = strlen(nome); i >= 0; i--){
-        
-        if(nome[i]==' '){
-            // printf("E: %d ", i);
-            e = i;
-            for(e++; e <strlen(nome); e++){
-                printf("%c", nome[e]);
-            }
-            printf('\n');
-            nome[i] = '\0';
-        }
-        if(nome[i]=='\0'){
-            f = i;
-        }
-    }
-    
-    return 0;
+	for (i=strlen(nome), aux=strlen(nome); i>=0; i--){
+		if (nome[i] == ' '){
+			j = i++;
+			while (j != aux){
+				printf("%c", nome[j]);
+				j++;
+			}
+			puts("");
+			aux = i;
+		}
+		
+	}
+
+    //imprime o primeiro nome
+	while (i!=aux){
+		printf("%c", nome[i]);
+		i++;
+	}
+
+  	return 0;
 }
